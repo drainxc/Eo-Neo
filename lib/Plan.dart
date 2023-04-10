f244f6f126ddbc0421764dd4f18192884ac9446c
ldh7228 <ldh7228@gmail.com>
2023-04-10 15:38:45 +0900

    Create Plan

--- lib/Plan.dart
+++ lib/Plan.dart
@@ -1,10 +1,14 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+
+import 'package:untitled1/Home.dart';
 
 void main() => runApp(const Plan());
 
 class Plan extends StatelessWidget {
-  const Plan({Key? key}) : super(key:key);
+  const Plan({Key? key}) : super(key: key);
 
   static const String _title = "Plan";
 
@@ -25,10 +29,26 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List planArr = [
+      {
+        "id": 0,
+        "plan": ["asdasd", 'sdfsdf', 'dfgdfg'],
+        "success": [false, false, false],
+      },
+      {
+        "id": 1,
+        "plan": ["asdasd", 'sdfsdf', 'dfgdfg'],
+        "success": [false, false, false],
+      },
+    ];
+
+
     return Scaffold(
       backgroundColor: Color(0xffFBC00A),
       body: Stack(
@@ -45,6 +65,43 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               ),
             ),
           ),
+          Positioned(
+            top: 100,
+            child: Column(children: [
+              Container(
+                width: width * 0.9,
+                height: 50,
+                child: Text('Plan',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xfff6f6f6),
+                    )),
+              ),
+              Container(
+                width: width * 1,
+                height: height * 0.74,
+                child: SafeArea(
+                    child: ScrollConfiguration(
+                      behavior: const ScrollBehavior().copyWith(overscroll: false),
+                      child: ListView.builder(
+                        padding: EdgeInsets.only(top: 0),
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                              title: Container(
+                                width: width * 1,
+                                height: 100,
+                                color: Color(0xfff6f6f6),
+                              )
+                          );
+                        },
+                      ),
+                    ),
+                )
+              ),
+            ]),
+          )
         ],
       ),
     );
