3b5daf1b5dac620b23ae5abcad55682eb3a33078
ldh7228 <ldh7228@gmail.com>
2023-04-10 15:38:59 +0900

    ADD

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/chart.dart';
 import 'package:untitled1/nav.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home:HomeScreen(),
     );
   }
 }
