0bd713f00f469c11b6d879910af03b5a9706f99b
ldh7228 <ldh7228@gmail.com>
2023-04-18 16:49:32 +0900

    Update publishing Setting Page

--- lib/Setting.dart
+++ lib/Setting.dart
@@ -4,7 +4,7 @@ import 'dart:math' as math;
 void main() => runApp(const Setting());
 
 class Setting extends StatelessWidget {
-  const Setting({Key? key}) : super(key:key);
+  const Setting({Key? key}) : super(key: key);
 
   static const String _title = "Plan";
 
@@ -30,7 +30,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Color(0xff72B42C),
+      backgroundColor: const Color(0xff72B42C),
       body: Stack(
         children: <Widget>[
           Positioned(
@@ -41,12 +41,209 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               child: Container(
                 width: 120,
                 height: 700,
-                color: Color(0xfff6f6f6),
+                color: const Color(0xfff6f6f6),
               ),
             ),
           ),
+          Positioned(
+              top: 100,
+              left: 0,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: width * 0.9,
+                    height: 50,
+                    child: const Text('Account',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xfff6f6f6),
+                        )),
+                  ),
+                  SizedBox(
+                    width: width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: width * 0.9,
+                          height: 300,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfff6f6f6),
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color(0xaa8AFFB2),
+                                blurRadius: 10.0,
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                width: width * 0.8,
+                                height: 70,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 52,
+                                      height: 52,
+                                      child: const Icon(
+                                        Icons.person,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const Text('Lee Dong Hyeon'),
+                                  ],
+                                ),
+                              ),
+                              DividerHr(width),
+                              SizedBox(
+                                width: width * 0.8,
+                                height: 70,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 52,
+                                      height: 52,
+                                      child: const Icon(
+                                        Icons.person_off,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const Text('Delete Account'),
+                                  ],
+                                ),
+                              ),
+                              DividerHr(width),
+                              SizedBox(
+                                width: width * 0.8,
+                                height: 70,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 52,
+                                      height: 52,
+                                      child: const Icon(
+                                        Icons.delete,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const Text('Lee Dong Hyeon'),
+                                  ],
+                                ),
+                              ),
+                              DividerHr(width),
+                              SizedBox(
+                                width: width * 0.8,
+                                height: 70,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 52,
+                                      height: 52,
+                                      child: const Icon(
+                                        Icons.logout,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const Text('Lee Dong Hyeon'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    width: width,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: width * 0.9,
+                          height: 60,
+                          child: Row(
+                            children: const [
+                              Text('Develop',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xfff6f6f6),
+                                  )),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: width * 0.9,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfff6f6f6),
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color(0xaa8AFFB2),
+                                blurRadius: 10.0,
+                              )
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                width: width * 0.8,
+                                height: 70,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 52,
+                                      height: 52,
+                                      child: const Icon(
+                                        Icons.code,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const Text('https://github.com/eastcopper/jang'),
+                                  ],
+                                ),
+                              ),
+                              DividerHr(width),
+                              SizedBox(
+                                width: width * 0.8,
+                                height: 70,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 52,
+                                      height: 52,
+                                      child: const Icon(
+                                        Icons.email_outlined,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    const Text('Email: ldh7228@gmail.com'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ))
         ],
       ),
+    );
+  }
+
+  Widget DividerHr(width) {
+    return Container(
+      height: 2,
+      width: width * 0.8,
+      color: Color(0xff72B42C),
     );
   }
 }
